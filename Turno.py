6cdb9fd792736205a6c793ea018594a10b911d9d
nicouema <nicouemacapdevila@gmail.com>
2022-06-13 16:21:04 -0300

    Update Turno.py

--- Turno.py
+++ Turno.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from CambioDeEstadoTurno import CambioDeEstadoTurno
+
 
 class Turno:
     def __init__(self, fechaGeneracion, diaSemana, fechaHoraInicio, fechaHoraFin) -> None:
@@ -29,6 +31,19 @@ class Turno:
     def cambiosDeEstadoTurno(self):
         return self._cambiosDeEstadoTurno
 
+    def crearCambioEstado(self, estado):
+        # Se crea un nuevo cambio de estado, con un estado que se pasa por parametro
+        cambioEstado = CambioDeEstadoTurno(estado)
+
+        # Se busca el estado actual (que ahora seria el anterior) y se
+        # setea su fechaHoraHasta con la fecha y hora actual
+        for cambioEstado in self.cambiosDeEstadoTurno:
+            if cambioEstado.sosEstadoActual():
+                cambioEstado.fechaHoraHasta = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+        # Finalmentese agrega el nuevo cambio de estado, ya con la fechaHoraHasta del estado anterior actualizada
+        self.cambiosDeEstadoTurno.append(cambioEstado)
+
     def sosPosteriorAFechaActual(self):
         if self.fechaHoraInicio > datetime.now():
             return True
@@ -43,5 +58,7 @@ class Turno:
                 return True
         return False
 
-    def reservar(self):
+    def reservar(self, estadoReservado):
+        self.crearCambioEstado(estadoReservado)
+
 
