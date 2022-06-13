8d8e8b69977fcc77e1e3dd217ee624c97a466ed3
nicouema <nicouemacapdevila@gmail.com>
2022-06-13 13:08:20 -0300

    Update CambioDeEstadoTurno.py

--- CambioDeEstadoTurno.py
+++ CambioDeEstadoTurno.py
@@ -1,10 +1,9 @@
 
 class CambioDeEstadoTurno():
-    def __init__(self, fechaHoraDesde, fechaHoraHasta) -> None:
+    def __init__(self, fechaHoraDesde, fechaHoraHasta, estado) -> None:
         self._fechaHoraDesde = fechaHoraDesde
         self._fechaHoraHasta = fechaHoraHasta
-        # TODO: Atributo Estado
-
+        self._estado = estado
     @property
     def fechaHoraDesde(self):
         return self._fechaHoraDesde
@@ -12,6 +11,10 @@ class CambioDeEstadoTurno():
     @property
     def fechaHoraHasta(self):
         return self._fechaHoraHasta
+
+    @property
+    def estado(self):
+        return self._estado
 
     def sosEstadoActual(self):
         if self._fechaHoraHasta is None:
