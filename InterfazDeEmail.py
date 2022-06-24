13623fb905e06e7879066ac411cf981524e28377
nicouema <nicouemacapdevila@gmail.com>
2022-06-24 16:10:44 -0300

    interfaces

--- InterfazDeEmail.py
+++ InterfazDeEmail.py
@@ -4,27 +4,12 @@ from datetime import date
 from datetime import datetime
 
 class InterfazDeEmail:
-    def __init__(self, email, turnoSelect):
-        self._email = email
-        self._turnoSelect = turnoSelect
 
-    @property
-    def email(self):
-        return self._email
-
-    @property
-    def turnoSeleccionado(self):
-        return self._turnoSelect
-
-    def enviarnotificacion(self):
-        messagebox.showinfo(title="Notificacion Enviada", message="Notificacion enviada a: " + self.email +
-                                                                  "\nDatos del turno: "
-                                                                  "\nFecha: " + self.turnoSeleccionado.fechaHoraInicio.date().strftime("%d/%m/%y") +
-                                                                  "\nHora de Inicio: " + self.turnoSeleccionado.fechaHoraInicio.time().strftime("%H:%M") +
-                                                                  "\nHora de Fin: " + self.turnoSeleccionado.fechaHoraFin.time().strftime("%H:%M"))
+    def enviarnotificacion(self, email, mensaje):
+        messagebox.showinfo(title="Notificacion Enviada a " + email, message=mensaje)
 
 
 if __name__ == '__main__':
     turno1 = Turno(datetime(2022, 6, 20, 4, 0, 0), 4, datetime(2022, 6, 30, 10, 0, 0), datetime(2022, 6, 30, 11, 0, 0), [])
-    interfazMail = InterfazDeEmail("nicouemacapdevila@gmail.com",turno1)
-    interfazMail.enviarnotificacion()
+    interfazMail = InterfazDeEmail()
+    interfazMail.enviarnotificacion("nicouemacapdevila@gmail.com",turno1)
---
+++ InterfazDeWhatsApp.py
@@ -0,0 +1,16 @@
+from tkinter import messagebox
+from Turno import Turno
+from datetime import date
+from datetime import datetime
+
+
+class InterfazDeWhatsApp:
+
+    def enviarnotificacion(self, numTelefono, mensaje):
+        messagebox.showinfo(title="Notificacion Enviada a " + numTelefono, message=mensaje)
+
+if __name__ == '__main__':
+    turno1 = Turno(datetime(2022, 6, 20, 4, 0, 0), 4, datetime(2022, 6, 30, 10, 0, 0), datetime(2022, 6, 30, 11, 0, 0), [])
+    interfazWp = InterfazDeWhatsApp()
+
+    interfazWp.enviarnotificacion("3543635023",turno1)
