95dea645590e878a15d4c286aff51b052e992c11
Sol Guzman <solguzman@emser.net>
2022-06-23 22:24:17 -0300

    .

--- InterfazDeTurno.py
+++ InterfazDeTurno.py
@@ -20,9 +20,9 @@ class InterfazDeReservaTurno():
     frame = tk.Frame(ventana)
     frame.pack()
     #Utils
-    def clear_window():
+    def clear_window(self):
         '''Destroy elements of a frame'''
-        for widget in frame.winfo_children():
+        for widget in self.frame.winfo_children():
             widget.destroy()
 
     #Métodos de clase
@@ -108,15 +108,15 @@ class InterfazDeReservaTurno():
     def mostrarDatosRTSeleccionado(self):
         self.labelframe_rtselec= tk.LabelFrame(self.frame,text='Recurso Tecnológico Seleccionado')
         #Headers
-        self.label_rtseleccionado.grid(row=0,column=0)
+        self.labelframe_rtselec.grid(row=0,column=0)
         self.label_rtseleccionadoNroInv = tk.Label(self.labelframe_rtselec, text='Nro Inventario')
         self.label_rtseleccionadoNroInv.grid(row=1,column=0)
         self.label_rtseleccionadoModMarca = tk.Label(self.labelframe_rtselec, text='Modelo y Marca')
         self.label_rtseleccionadoModMarca.grid(row=1,column=1)
         self.label_rtseleccionadoCI = tk.Label(self.labelframe_rtselec, text='Centro De Investigación')
-        self.label_rtseleccionadoCI.grid(row=1,column=3)
+        self.label_rtseleccionadoCI.grid(row=1,column=2)
         self.label_rtseleccionadoEstado = tk.Label(self.labelframe_rtselec, text='Estado')
-        self.label_rtseleccionadoCI.grid(row=1,column=4)
+        self.label_rtseleccionadoEstado.grid(row=1,column=3)
 
         sp = ttk.Separator(self.labelframe_rtselec, orient='horizontal')
         sp.grid(row=1,columnspan=4,sticky='ew')
@@ -124,9 +124,12 @@ class InterfazDeReservaTurno():
         self.cell_nroInvRTSeleccionado = self.rTSeleccionado['nroInv']
         self.cell_nroInvRTSeleccionado.grid(row=2,column=0)
         self.cell_ModMarcaRTSeleccionado = self.rTSeleccionado['modMarca']
-        self.cell_nroInvRTSeleccionado.grid(row=2,column=1)
-        self.cell_CIRTSeleccionado = self.cIDelRT['nombre']
+        self.cell_ModMarcaRTSeleccionado.grid(row=2,column=1)
+        self.cell_CIRTSeleccionado = self.cIDelRT.nombre
+        self.cell_CIRTSeleccionado.grid(row=2,column=2)
         self.cell_EstadoRTSeleccionado = self.rTSeleccionado['estadoActual']
+        self.cell_EstadoRTSeleccionado.grid(row=2,column=3)
+
         
 
 
--- hiFellowProgrammer.py
+++ hiFellowProgrammer.py
@@ -1,30 +1,36 @@
 """HELLO BUDDY! You might use this module to run some tests or leave comments for your fellow Programmers to see"""
-from struct import pack
-from textwrap import fill
+
 from tkinter import *
 from tkinter import ttk
 
+
+
 root = Tk()
+frame = Frame(root)
+frame.pack()
+def mostrarDatosRTSeleccionado(rTSeleccionado,cIDelRT):
+    labelframe_rtselec= LabelFrame(frame,text='Recurso Tecnológico Seleccionado')
+    #Headers
+    labelframe_rtselec.grid(row=0,column=0)
+    label_rtseleccionadoNroInv = Label(labelframe_rtselec, text='Nro Inventario')
+    label_rtseleccionadoNroInv.grid(row=1,column=0)
+    label_rtseleccionadoModMarca = Label(labelframe_rtselec, text='Modelo y Marca')
+    label_rtseleccionadoModMarca.grid(row=1,column=1)
+    label_rtseleccionadoCI = Label(labelframe_rtselec, text='Centro De Investigación')
+    label_rtseleccionadoCI.grid(row=1,column=2)
+    label_rtseleccionadoEstado = Label(labelframe_rtselec, text='Estado')
+    label_rtseleccionadoEstado.grid(row=1,column=3)
 
-def clear_window():
-    '''Destroy elements of a frame'''
-    for widget in ws.winfo_children():
-        widget.destroy()
-def openwin():
-    clear_window()
-    L = Label(ws,text='Hey world')
-    L2 = Label(ws,text='BYE BYE')
-    L.grid(row=0,column=0)
-    L2.grid(row=0,column=1)
-
-ws = Frame(root)
-ws.pack()
-L = Label(ws,text='1')
-L2 = Label(ws,text='2')
-sp = ttk.Separator(ws, orient=HORIZONTAL)
-sp.grid(row=1,columnspan=2,sticky='ew') #Stick from east to west
-L.grid(row=0,column=0)
-L2.grid(row=0,column=1)
-
+    sp = ttk.Separator(labelframe_rtselec, orient='horizontal')
+    sp.grid(row=1,columnspan=4,sticky='ew')
+    
+    cell_nroInvRTSeleccionado = rTSeleccionado['nroInv']
+    cell_nroInvRTSeleccionado.grid(row=2,column=0)
+    cell_ModMarcaRTSeleccionado = rTSeleccionado['modMarca']
+    cell_ModMarcaRTSeleccionado.grid(row=2,column=1)
+    cell_CIRTSeleccionado = cIDelRT.nombre
+    cell_CIRTSeleccionado.grid(row=2,column=2)
+    cell_EstadoRTSeleccionado = rTSeleccionado['estadoActual']
+    cell_EstadoRTSeleccionado.grid(row=2,column=3)
 
 root.mainloop()
